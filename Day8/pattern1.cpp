5b303d70e4c337da4305d86ca1fa453d87e5e17d
MANISHA BOSE <manishabose.2001@gmail.com>
2025-01-15 22:45:33 +0530

    Day8: 4 [Pattern 1]

---
+++ Day8/pattern1.cpp
@@ -0,0 +1,22 @@
+/*
+Sample Input 1:
+3
+Sample Output 1:
+* * *
+* * *
+* * *
+*/
+#include <iostream>
+using namespace std;
+void nForest(int n)
+{
+    // Write your code here.
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}

8da1773927a4c9fa00cd91547b820fe74dd5d0dd
MANISHA BOSE <manishabose.2001@gmail.com>
2025-02-01 12:54:08 +0530

    Day19: 1

---
+++ Day19/LCdailyProb.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    bool isArraySpecial(vector<int> &nums)
+    {
+        if (nums.size() < 2)
+            return true;
+        int e_o = nums[0] % 2; // even = 0, odd = 1;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (e_o != nums[i] % 2)
+                e_o = nums[i] % 2;
+            else
+                return false;
+        }
+        return true;
+    }
+};

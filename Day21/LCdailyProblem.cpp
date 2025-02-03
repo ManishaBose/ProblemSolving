72d72dfab9822ddca489571f945f1cd2cca2cbde
MANISHA BOSE <manishabose.2001@gmail.com>
2025-02-03 23:49:04 +0530

    Day 21: Daily problem

--- Day21/LCdailyProblem.cpp
+++ Day21/LCdailyProblem.cpp
@@ -1,3 +1,30 @@
 // LC: 3105
 #include <iostream>
 using namespace std;
+class Solution
+{
+public:
+    int longestMonotonicSubarray(vector<int> &nums)
+    {
+        int ans = INT_MIN;
+        int l = 1;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] < nums[i + 1])
+                l++;
+            else
+                l = 1;
+            ans = max(ans, l);
+        }
+        l = 1;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] > nums[i + 1])
+                l++;
+            else
+                l = 1;
+            ans = max(ans, l);
+        }
+        return max(ans, 1);
+    }
+};
